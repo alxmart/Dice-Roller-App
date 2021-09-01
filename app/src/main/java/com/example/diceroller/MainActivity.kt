ed4863224bda51aa3d1f935b23ddbd63fed303eb
alxmart <alxmartinez@gmail.com>
2021-09-01 16:54:40 -0300

    Adding images and define setImageResources.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -10,27 +11,31 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-
         rollButton.text = "ROLL"
-
         rollButton.setOnClickListener {
-
-            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-
-            rollDice()
+           rollDice()
         }
     }
 
     private fun rollDice() {
 
-        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
 
-        val ramdomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        resultText.text = ramdomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+            diceImage.setImageResource(drawableResource)
+
     }
 }
